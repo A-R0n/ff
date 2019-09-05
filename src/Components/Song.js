9cca7411fdcfdf59af68a8f304d8846bb4c6315a
Aaron Estes <aestesc@gmail.com>
2019-09-04 22:58:14 -0500

    song

--- src/Components/Song.js
+++ src/Components/Song.js
@@ -4,7 +4,7 @@ import ReactAudioPlayer from 'react-audio-player';
 class Song extends Component {
 	render() {
         return <ReactAudioPlayer
-        // src="https://ffavi.s3.amazonaws.com/Lil+Wayne+-+3+peat.mp3"
+        src="https://ffavi.s3.amazonaws.com/Lil+Wayne+-+3+peat.mp3"
         autoPlay
         controls />;
 	}
